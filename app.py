d5eeaa1647ee56f2fa82380b18e3bcaa7e353160
arufak <121422213+arufak@users.noreply.github.com>
2025-09-20 07:53:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -234,4 +234,6 @@ def load_html(filename):
     return send_from_directory('html', filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
